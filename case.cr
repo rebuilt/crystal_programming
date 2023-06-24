05341f01736171581e7000ab53f1a84da1af78c1
rebuilt <memoryman51@hotmail.com>
2023-06-23 21:05:00 -0700

    basic

---
+++ case.cr
@@ -0,0 +1,10 @@
+case Time.local.month
+when 1, 2, 3
+  puts "We a on the first quarter"
+when 4, 5, 6
+  puts "We a on the second quarter"
+when 7, 8, 9
+  puts "We a on the third quarter"
+when 10, 11, 12
+  puts "We a on the fourth quarter"
+end
---
+++ hello_name.cr
@@ -0,0 +1,8 @@
+def get_name
+  print "What's your name? "
+  read_line
+end
+
+puts "Hello, #{get_name}!" 
+number = 1232_i64
+puts number
